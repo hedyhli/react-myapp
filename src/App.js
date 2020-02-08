424cd74229a40bad7babccb1f7bd516881d85131
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-02-08 14:31:07 +0800

    add prop-"name" validation if nothing provided for the prop "name", then put it as "world"

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class HelloName extends React.Component { // user-def always capitalise first letter!
+  constructor(props) {
+    super(props);
+    this.props.name ? this.name = this.props.name : 
+    this.name = "World" // if nothing provided then set it as "World"
+  }
   render() {
-    return <h1>Hello, {this.props.name}!</h1>;
+    return <h1>Hello, {this.name}!</h1>;
   }
 }
 
 function App() {
-  var helloWorld = <HelloName name="World"/>;
+  var helloWorld = <HelloName name=""/>;
   ReactDOM.render(
     helloWorld,
     document.getElementById('root')
