67d382ead26a283d4d6b0488bfb8a80f004401a7
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-02-09 19:51:38 +0800

    changed from props to states

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 class HelloName extends React.Component { // user-def always capitalise first letter!
   constructor(props) {
     super(props);
-    this.props.name ? this.name = this.props.name : 
-    this.name = "World" // if nothing provided then set it as "World"
+    this.state = {
+      name: ""
+    }
+    this.props.name ? this.state.name = this.props.name : 
+    this.state.name = "World" // if nothing provided then set it as "World"
   }
   render() {
-    return <h1>Hello, {this.name}!</h1>;
+    return <h1>Hello, {this.state.name}!</h1>;
   }
 }
 
