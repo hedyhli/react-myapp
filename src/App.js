2534f70da4f3201cdb13d203a424b158784d4b4f
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-02-11 20:53:29 +0800

    attempt to add new component + use (didn't work)
    attempt to add new component + use (didn't work)

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ class HelloName extends React.Component { // user-def always capitalise first le
   constructor(props) {
     super(props);
     this.state = {
-      name: ""
+      name: "World"
     }
-    this.props.name ? this.state.name = this.props.name : 
-    this.state.name = "World" // if nothing provided then set it as "World"
+    if (this.props.name) {
+      this.setState({name: this.props.name});
+    }
+    // if nothing provided then let it still be "World"
   }
   render() {
     return <h1>Hello, {this.state.name}!</h1>;
   }
 }
 
+class BodyStuff extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: "Sup, I'm the body"
+    }
+    if (this.props.contents) {
+      this.setState({contents: this.props.contents});
+    }
+  }
+  render() {
+    return <div id="body-stuff"><h1>{this.state.contents}!</h1></div>;
+  }
+}
+
 function App() {
   var helloWorld = <HelloName name=""/>;
+  var myBody = <BodyStuff contents="Hi"/> // TODO - put this on page!
   ReactDOM.render(
     helloWorld,
     document.getElementById('root')
