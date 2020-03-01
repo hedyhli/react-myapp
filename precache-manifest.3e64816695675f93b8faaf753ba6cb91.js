bda540a7f140c316294aa1c90778b3d1f8419514
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-03-01 15:47:52 +0800

    Updates

---
+++ precache-manifest.3e64816695675f93b8faaf753ba6cb91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bf38cb5b553c2bd96f7eca65d05a94",
+    "url": "/hedyhyry/react-myapp.git/index.html"
+  },
+  {
+    "revision": "c780b340fb5064e83b2c",
+    "url": "/hedyhyry/react-myapp.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99bf9de4337544d4301b",
+    "url": "/hedyhyry/react-myapp.git/static/js/2.401238ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hedyhyry/react-myapp.git/static/js/2.401238ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c780b340fb5064e83b2c",
+    "url": "/hedyhyry/react-myapp.git/static/js/main.fec93961.chunk.js"
+  },
+  {
+    "revision": "8e0605a08d9767b87d43",
+    "url": "/hedyhyry/react-myapp.git/static/js/runtime-main.6e260ad3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hedyhyry/react-myapp.git/precache-manifest.3e64816695675f93b8faaf753ba6cb91.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hedyhyry/react-myapp.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
