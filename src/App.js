35361083c3bcb43c9d4c5720f1b3a9b5670f23bc
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-03-14 13:24:22 +0800

    add TODO and FIXME comments

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends React.Component {
   render() {
     var helloHeader = <HelloName name=""/>;
     var headerDescrpt = <HeaderDescrpt contents="Hi"/>;
-    var myList = <ListsNKeys items={[5,4,3,2,1]}/>
-    return ( // return both components
+    var myList = <ListsNKeys items={[5,4,3,2,1]}/> // FIXME:props not working (showing default items)
+    return ( // TODO: add "list: " title before the list
       <div>
         {helloHeader}
         {headerDescrpt}
